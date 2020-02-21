dfcf88decb1b10c6b034545e1518935474882910
Marta <mgolabek@inovex.de>
2020-02-21 20:00:32 +0100

    dit structure created by cookiecutter package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The aim of this project is to create an LSTM-based model to produce personalized bday wishes in multiple languages (Russian, Polish, German, English)',
+    author='lstm_freaks',
+    license='MIT',
+)

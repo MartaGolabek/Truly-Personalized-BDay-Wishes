5e3bbe958f4675157e75644ec33289fb7b8bc6cb
Marta <mgolabek@inovex.de>
2020-02-23 18:55:50 +0100

    Second data set - bday wishes for friend - this time using scrapy

---
+++ src/data/bday_wishes_friend_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+import csv
+
+class BdayWishesFriendSpider(scrapy.Spider):
+    name = "bday_wishes_friend"
+    start_urls = ["https://therightmessages.com/short-and-long-birthday-wishes-for-best-friend/"]
+
+    def parse(self, response):
+        set_selector = 'p::text'
+
+        raw_wishes = ["text"]
+        for record in response.css(set_selector):
+            print(record.get())
+            raw_wishes.append(record.get())
+
+        filename = "../../data/raw/bday_wishes_for_friend_2.csv"
+        with open(filename, "w", newline="") as file:
+            csv_writer = csv.writer(file, delimiter=";")
+            for record in raw_wishes:
+                csv_writer.writerow([record])
+        self.log('Saved file %s' % filename)

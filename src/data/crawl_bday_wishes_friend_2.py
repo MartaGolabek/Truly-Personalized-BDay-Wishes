03ac6034347d1b888f05bdd4f0c30d35c3bdbe10
Marta <mgolabek@inovex.de>
2020-02-23 22:09:04 +0100

    3rd data set bday wishes for a friend

---
+++ src/data/crawl_bday_wishes_friend_2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+def main():
+    """ Crawls a website, parse bday wishes and saves them to a file.
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('crawling and parsing a website')
+
+    # crawl bday wishes for a friend
+    __website_address = "https://www.holidaycardsapp.com/wishes/best-happy-birthday-wishes-for-friends/"
+    r = requests.get(__website_address)
+
+    doc = BeautifulSoup(r.text, "html.parser")
+
+    raw_wishes = ["text"]
+    for record in doc.find_all("font"):
+        if re.match("\d+\.\s", record.text) is not None:
+            temp_1 = re.sub(r"\d+\.", "", record.text).strip()
+            raw_wishes.append(temp_1)
+
+    with open("../../data/raw/bday_wishes_for_friend_3.csv", "w", newline="") as file:
+        csv_writer = csv.writer(file, delimiter=";")
+        for record in raw_wishes:
+            print(record)
+            csv_writer.writerow([record])
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
